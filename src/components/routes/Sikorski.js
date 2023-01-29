40bb037387cc9ed80a0bc63950a73548d70673d5
jarekmadczak <miszorjest@gmail.com>
2023-01-29 21:26:41 +0100

    first

--- src/components/routes/Sikorski.js
+++ src/components/routes/Sikorski.js
@@ -2,6 +2,34 @@ import React from "react";
 import Welcome from "../pages/Welcome";
 import Header from "../pages/Header";
 import Sliders from "../pages/Sliders";
+
+class Foo extends React.Component {
+  constructor(){
+    super();
+    this.state = {loaded: false};
+  }
+
+  render(){
+    return (
+      <div>
+        {this.state.loaded ? null :
+          <div
+            style={{
+              background: 'red',
+              height: '400px',
+              width: '400px',
+            }}
+          />
+        }
+        <img
+          style={this.state.loaded ? {} : {display: 'none'}}
+          src={this.props.src}
+          onLoad={() => this.setState({loaded: true})}
+        />
+      </div>
+    );
+  }
+}
 const Sikorski = () =>{
     return (
     <div >
