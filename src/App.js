ce2b036410abd769e5fa5f6e44e9d05d35fcd57a
jarekmadczak <miszorjest@gmail.com>
2023-01-29 03:42:12 +0100

    site

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.scss";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Sikorski from "./components/routes/Sikorski";
 import Navigation from "./components/routes/Navigation";
 
 function App() {
   return (
     <div>
-      <Sikorski />
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigation />} />
+        <Route path="sikorski" element={<Sikorski />} /> 
+      </Routes>
+    </BrowserRouter>
  
     </div>
   );
